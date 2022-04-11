3a35a46998bf8b254496968fc5de0536ae19d1c5
kirill <kiryl_lysiakou@senla.eu>
2022-04-11 22:52:44 +0300

    - get, update issues done

--- backend/src/main/java/edu/grsu/web2/backend/rest/StatusRepository.java
+++ backend/src/main/java/edu/grsu/web2/backend/rest/StatusRepository.java
@@ -4,7 +4,10 @@ import edu.grsu.web2.backend.entity.Status;
 import edu.grsu.web2.backend.projections.StatusView;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@CrossOrigin(methods = {RequestMethod.GET, RequestMethod.DELETE, RequestMethod.PATCH, RequestMethod.POST, RequestMethod.PUT})
 @RepositoryRestResource(collectionResourceRel = "statuses", path = "statuses", excerptProjection = StatusView.class)
 public interface StatusRepository extends CrudRepository<Status, Integer> {
 }
